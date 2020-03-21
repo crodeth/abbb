ed6dea9b2da24a74d275fb32ed6051b4e912155e
crodeth <62470243+crodeth@users.noreply.github.com>
2020-03-21 22:02:30 +0900

    Add files via upload

---
+++ abbb.py
@@ -0,0 +1,23 @@
+import discord
+
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("상태메시지")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("*안녕"):
+        await message.channel.send("코딩좀 도와줘ㅠㅠㅠ")
+
+
+client.run("NjkwODg0MjQ5OTM0MzY0Njkz.XnX7nw.52l7DR25CNEULqM7MZOKFHHYbtQ")
